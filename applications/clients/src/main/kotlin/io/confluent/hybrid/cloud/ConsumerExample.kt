c9726847a9ea4cb49d9069fd824add5fde412de0
Victor Cosqui <vcosqui@confluent.io>
2022-11-16 16:51:21 +0100

    simplify and cleanup consumer

--- applications/clients/src/main/kotlin/io/confluent/hybrid/cloud/ConsumerExample.kt
+++ applications/clients/src/main/kotlin/io/confluent/hybrid/cloud/ConsumerExample.kt
@@ -2,29 +2,25 @@
 
 package io.confluent.hybrid.cloud
 
-import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import org.apache.avro.generic.GenericRecord
-import org.apache.kafka.clients.consumer.ConsumerConfig.*
+import org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.serialization.StringDeserializer
 import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.slf4j.LoggerFactory.getLogger
 import uk.org.webcompere.lightweightconfig.ConfigLoader.loadPropertiesFromResource
 import java.time.Duration.ofMillis
 import java.util.UUID.randomUUID
 
-private val log: Logger = LoggerFactory.getLogger("ConsumerExample")
+private val log: Logger = getLogger("io.confluent.hybrid.cloud.ConsumerExample")
 
 fun main() {
     val topic = "customers"
-    val props = loadPropertiesFromResource("client.properties")
-    props[KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
-    props[VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAvroDeserializer::class.java.name
-    props[GROUP_ID_CONFIG] = "confluent_cli_consumer_1${randomUUID()}"
-    props[AUTO_OFFSET_RESET_CONFIG] = "earliest"
+    val props = loadPropertiesFromResource("client.properties").let {
+        it[GROUP_ID_CONFIG] = "confluent_cli_consumer_1${randomUUID()}"
+        it
+    }
 
     val consumer = KafkaConsumer<String, GenericRecord>(props).apply { subscribe(listOf(topic)) }
-
     consumer.use {
         while (true) {
             consumer.poll(ofMillis(100)).forEach { log.info("${it.value()}") }
